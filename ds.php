2342a21cdf7c7efc4c375ecc30990a65be07023d
Dmytro Filiuk <49027390+DmytroFiliuk2@users.noreply.github.com>
2021-11-10 10:21:24 +0200

    Create ds.php
    2D Array - DS

---
+++ ds.php
@@ -0,0 +1,14 @@
+<?php
+
+function hourglassSum($arr)
+{
+    for ($i = 0; $i <= count($arr) - 3; $i++) {
+        for ($j = 0; $j <= count($arr[$i]) - 3; $j++) {
+            $result[] = $arr[$i][$j] + $arr[$i][$j + 1] + $arr[$i][$j + 2]
+                + $arr[$i + 1][$j + 1]
+                + $arr[$i + 2][$j] + $arr[$i + 2][$j + 1] + $arr[$i + 2][$j + 2];
+        }
+    }
+
+    return max($result);
+}
